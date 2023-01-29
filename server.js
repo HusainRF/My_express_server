f6e92d678e2bbbd3b0a705b360002d36c0fbad4e
HusainRF <justgetupanddoit9@gmail.com>
2023-01-29 21:38:19 +0530

    express_server: learned about multiple routes

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ app.get("/" ,function(req, res){
     res.send("<h1> Hello World!!.......");
 });
 
-app.listen(3001,function(){
+app.get("/contact" ,function(req, res){
+    res.send("<h1> Husain@gmail.com");
+});
+
+app.get("/about" ,function(req, res){
+    res.send("<h1>I am Husain</h1><p> I am a programmer ans I am learning the express for node js. I am currently in a third year of my college medi-caps University.</p><br><p>That's all about me THhank You!!</p>");
+});
+
+app.listen(3000,function(){
     console.log("server started on port 3000");
 });
