8917b879c9707f33bb15b94b78c2a26314546af4
HusainRF <justgetupanddoit9@gmail.com>
2023-01-29 18:31:25 +0530

    express-server console.log(req)

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.get("/" ,function(req, res){
+    console.log(req);
+});
+
 app.listen(3001,function(){
     console.log("server started on port 3000");
 });
