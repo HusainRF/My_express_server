17b1bf0ba402e12c74711e131915b5181e643006
HusainRF <justgetupanddoit9@gmail.com>
2023-01-31 22:20:47 +0530

    express - learn about body parser and how it help to read the value of html attribute(name) which act as a variable

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({extended : true}));
 
 app.get("/" , function(req , res){
     res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/" , function(req , res){
-    res.send("Thanks for yout post");
+    // req.body.num1 represent the num1 which is the name of input item in index.html file
+    // console.log(req.body.num1);
+    // console.log(req.body.num2);
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1 + num2;
+    res.send("the result of the calculation is " + result);
 });
 
 app.listen(3000, function(){
