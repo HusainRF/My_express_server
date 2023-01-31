69e09689531310edfa10f7d28fb57cfa42e11353
HusainRF <justgetupanddoit9@gmail.com>
2023-01-31 23:23:40 +0530

    express: created BMI calculator on a new port 3000/bmicalculator

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -18,6 +18,18 @@ app.post("/" , function(req , res){
     res.send("the result of the calculation is " + result);
 });
 
+app.get("/bmicalculator" , function(req, res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator" , function(req, res){
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+    console.log(weight);
+    var bmi = ( weight / (height *height));
+    res.send("Your BMI is " + bmi);
+});
+
 app.listen(3000, function(){
     console.log("server started for  port 3000");
 });
