546f13b82b50bcb4ff2e434da602f73c7b40411f
HusainRF <justgetupanddoit9@gmail.com>
2023-01-31 20:50:32 +0530

    the post method implimented

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -5,6 +5,10 @@ app.get("/" , function(req , res){
     res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/" , function(req , res){
+    res.send("Thanks for yout post");
+});
+
 app.listen(3000, function(){
     console.log("server started for  port 3000");
 });
